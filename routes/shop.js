52dfc468e2d53c520e904c2ddae730199a8c7709
LahiruSen <lahiru.16@cse.mrt.ac.lk>
2024-04-14 19:54:19 +0530

    innitial commit

---
+++ routes/shop.js
@@ -0,0 +1,13 @@
+const path = require('path');
+const productsController = require('../controllers/products');
+
+const express = require('express');
+
+const rootDir = require('../util/path');
+const adminData = require('./admin');
+
+const router = express.Router();
+
+router.get('/', productsController.getProducts);
+
+module.exports = router;
