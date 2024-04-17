a51709a9c92c183f622454d471e48ccf9fedba6e
LahiruSen <lahiru.16@cse.mrt.ac.lk>
2024-04-17 23:26:48 +0530

    Configured json file as the data source.

--- models/product.js
+++ models/product.js
@@ -1,3 +1,6 @@
+const rootDir = require('../util/path');
+const path = require('path');
+const fs = require('fs');
 const products = [];
 
 module.exports = class Product{
@@ -6,11 +9,29 @@ module.exports = class Product{
     }
 
     save(){
+        const p = path.join(rootDir, 'data','products.json');
+        fs.readFile(p, (err, fileContent) => {
+            let products = [];
+            if (!err){
+                products = JSON.parse(fileContent);
+            }
+            products.push(this);
+            fs.writeFile(p, JSON.stringify(products), (err) =>{
+                console.log(err);
+            })
+        })
+
         products.push(this);
     }
 
     static fetchAll(){
-        return products;
+        const p = path.join(rootDir, 'data','products.json');
+        fs.readFile(p,(err, fileContent) => {
+            if (err){
+                return [];
+            }
+            return JSON.parse(fileContent);
+        } );
     }
 }
 
