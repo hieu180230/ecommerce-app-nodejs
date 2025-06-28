2ac13e914db6ae092d0c5551684169b410929ba1
Cookie <hieu180230@gmail.com>
2025-06-28 16:44:29 +0700

    Update database.js

--- util/database.js
+++ util/database.js
@@ -1,7 +1,7 @@
 const Sequelize = require("sequelize");
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-  dialect: process.env.DB_DIALECT,
+  dialect: "mysql",
   host: process.env.DB_HOST
 });
 
