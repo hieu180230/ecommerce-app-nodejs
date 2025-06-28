758e85c47f9236fb5b5b1a4deca35ad7011a5d8a
Cookie <hieu180230@gmail.com>
2025-06-28 17:17:20 +0700

    update database

--- util/database.js
+++ util/database.js
@@ -1,8 +1,17 @@
+require('dotenv').config();
 const Sequelize = require("sequelize");
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  port: 3306,
+  logging: console.log,
   dialect: "mysql",
-  host: process.env.DB_HOST
+  host: process.env.DB_HOST,
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 60000, // Increase from default 30000ms
+    idle: 10000,
+  }
 });
 
 module.exports = sequelize;
